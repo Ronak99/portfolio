c747d0040811f978c53778038439f516e4b8ba8c
Ronak99 <punase.ronak99@gmail.com>
2026-07-09 11:34:45 +0530

    chore: remove faulty chess puzzle

--- data/chessPuzzles.ts
+++ data/chessPuzzles.ts
@@ -3,7 +3,6 @@ export const CHESS_PUZZLES: string[] = [
   "6k1/5ppp/8/8/8/8/8/R6K w - - 0 1", // Ra8#
   "6k1/5ppp/8/8/8/8/5PPP/4Q1K1 w - - 0 1", // Qe8#
   "7k/8/6QK/8/8/8/8/8 w - - 0 1", // Qg7#
-  "6k1/8/6K1/8/8/8/8/7R w - - 0 1", // Rh8#
 ];
 
 export function randomPuzzleIndex(exclude?: number): number {
